4c32624fa345a4cc29baf40edb35634c7591b21a
José Lucas <gomessze03@gmail.com>
2026-03-06 20:52:03 -0300

    chore: Configura estrutura de pastas e paleta de cores globais

---
+++ src/styles/colors.ts
@@ -0,0 +1,10 @@
+export const COLORS = {
+  vermelhoPrincipal: "#A90000",
+  preto: "#000000",
+  branco: "#FFFFFF",
+
+  cinzaFundo: "#F0F2F5",
+  cinzaCard: "#FFFFFF",
+  textoPrincipal: "#333333",
+  textoSecundario: "#666666",
+};
