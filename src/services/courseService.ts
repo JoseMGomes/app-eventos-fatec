fbd9f0402558387fed3a97331d315308f7dd2322
José Lucas <141762275+JoseMGomes@users.noreply.github.com>
2026-04-11 14:54:02 -0300

    feat: Integração do gerenciamento de usuários e cursos com a API NestJS

---
+++ src/services/courseService.ts
@@ -0,0 +1,31 @@
+import { api } from '../factory/api';
+import { authService } from './authService';
+
+export const courseService = {
+  getAll: async () => {
+    try {
+      return await api.get('/courses');
+    } catch (error: any) {
+      if (error.response && error.response.status === 401) {
+        await authService.getCSRF(); 
+        return await api.get('/courses');
+      }
+      throw error;
+    }
+  },
+
+  create: async (dados: { name: string }) => {
+    await authService.getCSRF();
+    return await api.post('/courses/create', dados);
+  },
+
+  update: async (id: string | number, dados: { name: string }) => {
+    await authService.getCSRF();
+    return await api.patch(`/courses/patch/${id}`, dados);
+  },
+
+  delete: async (id: string | number) => {
+    await authService.getCSRF();
+    return await api.delete(`/courses/delete/${id}`);
+  }
+};
